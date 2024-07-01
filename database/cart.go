955ef3021dbc664e86dd6c3ae61bb3c08d8270b2
jopaleti <oopaletijohnson@gmail.com>
2024-07-01 23:10:36 +0100

    ...

--- database/cart.go
+++ database/cart.go
@@ -64,7 +64,52 @@ func BuyItemFromCart(ctx context.Context, userCollection *mongo.Collection, user
 	// create an order with the items
 	// Empty upp the cart
 
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
 
+	var getcartitems models.User
+	var ordercart models.Order
+
+	ordercart.Order_ID = primitive.NewObjectID()
+	ordercart.Order_At = time.Now()
+	ordercart.Order_Cart = make([]models.ProductUser, 0)
+	ordercart.Payment_Method.COD = true
+
+	unwind := bson.D{{Key: "$unwind", Value:bson.D{primitive.E{Key:"path", Value:"$usercart"}}}}
+	grouping := bson.D{{Key: "$group", Value:bson.D{primitive.E{Key:"_id", Value:"$_id"}, {Key:"total", Value: bson.D{primitive.E{Key: "$sum", Value:"$usercart.price"}}}}}}
+	currentresults, err := userCollection.Aggregate(ctx, mongo.Pipeline{unwind, grouping})
+	ctx.Done()
+
+	if err != nil {
+		panic(err)
+	}
+
+	var getusercart []bson.M
+	if err = currentresults.All(ctx, &getusercart); err != nil {
+		panic(err)
+	}
+	var total_price int32
+
+	for _, user_item := range getusercart {
+		price = user_item["total"]
+		total_price = user_item[int32]
+	}
+	ordercart.Price = int(total_price)
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key:"orders", Value: ordercart}}}}
+	_, err = userCollection.UpdateMany(ctx, filter, update)
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = userCollection.FindOne(ctx, bson.D{primitive.E{Key:"_id", Value:id}}).Decode(&getcartitems)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func InstantBuy() {
