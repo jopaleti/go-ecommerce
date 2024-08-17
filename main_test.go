d5f8eb6e79caea3c9e7d9050847e8469fd175ed0
jopaleti <oopaletijohnson@gmail.com>
2024-08-18 00:07:29 +0100

    TestLogin

--- main_test.go
+++ main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jopaleti/go-ecommerce/controllers"
 	"github.com/jopaleti/go-ecommerce/helpers"
 	"github.com/stretchr/testify/assert"
 )
@@ -36,4 +39,28 @@ func TestHomeHandler(t *testing.T) {
 	assert.Equal(t, mockResponse, string(responseData))
 
 	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+type userModel struct {
+	Email	string	`json:"email"      validate:"email,required"`
+	Password string	`json:"password"   validate:"required,min=6"`
+}
+
+func TestLogin (t *testing.T) {
+
+	r := helpers.SetUpRouter()
+	r.POST("/users/login", controllers.Login())
+
+	user := userModel{
+		Email: "segunopaleti@gmail.com",
+		Password: "tobi1234",
+	}
+	
+	jsonValue, _ := json.Marshal(user)
+	req, _ := http.NewRequest("POST", "/users/login", bytes.NewBuffer(jsonValue))
+
+	w := httptest.NewRecorder()
+	
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusFound, w.Code)
 }
